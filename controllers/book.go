960d193de6fd2309b6da4c812946adefb13343fa
Bruno Marques <blmarques.dev@gmail.com>
2022-09-03 19:53:15 -0300

    feat(get-books): implementes method of get all books

--- controllers/book.go
+++ controllers/book.go
@@ -1,9 +1,24 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
+
+	"github.com/blmarquess/go-books-api/database"
+	"github.com/blmarquess/go-books-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func GetAllBooks(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{
-		"message": "GetAllBooks",
-	})
+	db := database.GetDBInstance()
+	var books []models.Book
+	err := db.Find(&books).Error
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Internal Server Error",
+		})
+		log.Println(err)
+		return
+	}
+	ctx.JSON(http.StatusOK, books)
 }
